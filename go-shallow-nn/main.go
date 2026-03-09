34b1a0d76341fabd14ebdc31ce5c60e1c0ce67c1
Pedram <pedram.ep@gmail.com>
2026-03-09 15:56:21 +0330

    add `computeMeanStd` function

--- go-shallow-nn/main.go
+++ go-shallow-nn/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 
@@ -63,6 +64,33 @@ func loadCSV(path string, hasOutcome bool) (*mat.Dense, []float64, []string, err
 	return features, outcomes, header, nil
 }
 
+func computeMeanStd(m *mat.Dense) ([]float64, []float64) {
+	r, c := m.Dims()
+	means := make([]float64, c)
+	stds := make([]float64, c)
+
+	for j := 0; j < c; j++ {
+		col := mat.Col(nil, j, m)
+		sum := 0.0
+		for _, val := range col {
+			sum += val
+		}
+		means[j] = sum / float64(r)
+	}
+
+	for j := 0; j < c; j++ {
+		col := mat.Col(nil, j, m)
+		var sumSq float64
+		for _, v := range col {
+			diff := v - means[j]
+			sumSq += diff * diff
+		}
+		stds[j] = sqrt(sumSq / float64(r))
+	}
+	return means, stds
+}
+
+func sqrt(x float64) float64 { return math.Sqrt(x) }
 
 func main() {
 	trainFeatures, _, header, err := loadCSV("data/diabetes_train.csv", true)

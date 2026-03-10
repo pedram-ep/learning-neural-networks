10004a8365c6f2f4cd95d6f149727aab07ba6437
Pedram <pedram.ep@gmail.com>
2026-03-10 17:23:42 +0330

    add `validation.go` - add full implementation of different metrics (Precision, Recall, Accuracy, F1) - add function to print the results of those metrics

--- go-shallow-nn/main.go
+++ go-shallow-nn/main.go
@@ -23,7 +23,6 @@ func main() {
 	trainNorm := standardize(trainFeatures, means, stds)
 	testNorm := standardize(testFeatures, means, stds)
 
-	// Adding bias term to both train and test
 	trainBias := addBias(trainNorm)
 	testBias := addBias(testNorm)
 
@@ -38,29 +37,19 @@ func main() {
 	X_train = mat.DenseCopyOf(X_train_row.T())
 	X_val = mat.DenseCopyOf(X_val_row.T())
 
+	// Training the mmodel
 	inputSize := X_train.RawMatrix().Rows
 	hiddenSize := 1000
 	model := NewModel(inputSize, hiddenSize)
 
 	model.Fit(X_train, y_train, 0.01, 200)
 
+	// Compact metrics printing
 	trainAcc := evaluate(model, X_train, y_train)
 	valAcc := evaluate(model, X_val, y_val)
 	fmt.Printf("Training Accuracy: %.2f%%\n", trainAcc)
 	fmt.Printf("Validation Accuracy: %.2f%%\n", valAcc)
 
-	// X_test := mat.DenseCopyOf(testBias.T())
-
-	// _, A2_test := model.Predict(X_test)
-	// preds := make([]float64, A2_test.RawMatrix().Cols)
-	// positivePreds := 0
-	// for i := range preds {
-	// 	if A2_test.At(0, i) > 0.5 {
-	// 		preds[i] = 1
-	// 		positivePreds += 1
-	// 	}
-	// }
-	
-
-	// fmt.Printf("Positive predictions on test: %d/%d\n", positivePreds, len(preds))
+	// Full metrics printing
+	printMetrics(model, X_train, X_val, y_train, y_val, 0.5)
 }
